65dc0c8458c954ce0ad1689517437c68726115ac
Sahil Thapa <thapasahil997@gmail.com>
2023-09-07 09:32:00 +0300

    change favicon, change app name

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,10 +6,10 @@ const Homepage = () => {
         <div style={{minHeight:'100vh'}}>
           <header style={{ paddingLeft: 0, paddingTop: '1px' }}>
           <div className="p-5 text-center bg-light" style={{marginTop: '58px'}}>
-          <h1 className="mb-3">Heading</h1>
-          <h4 className="mb-3">Subheading</h4>
+          <h1 className="mb-3">Shopply</h1>
+          <h4 className="mb-3">You demand, We supply</h4>
           <a className="btn btn-primary" href="/products" role="button">
-            Call to action
+           Products
           </a>
         </div>
         </header>
